1a52d5daabbe73a0b12fe752639a51e488f4545c
fork71enthropy <julientelook@gmail.com>
2026-02-26 12:58:14 +0100

    admin.py in all apps

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Category,Post,Like_post
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Post)
+admin.site.register(Like_post)
+
