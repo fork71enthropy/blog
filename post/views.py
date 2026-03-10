394378eaa1eae09d62a6f7fb761fb1adf601d35e
fork71enthropy <julientelook@gmail.com>
2026-03-10 21:36:20 +0100

    first publication

--- post/views.py
+++ post/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Post,Profile
+import markdown
 # Create your views here.
+
+
+def post_detail(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    post.content_post = markdown.markdown(post.content_post,extensions=['fenced_code','codehilite'])
+    return render(request, 'post_detail.html', {'post': post})
+
+def acceuil(request):
+    return render(request,'acceuil.html')
+
+
