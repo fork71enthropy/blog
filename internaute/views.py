f607131ff6bbb26e8b1f1f2054aba176cd24b78a
fork71enthropy <julientelook@gmail.com>
2026-03-20 15:55:35 +0100

    internaute/views.py

--- internaute/views.py
+++ internaute/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Internaute
+from .forms import InscriptionForm
+from django.core.mail import send_mail,BadHeaderError
+from django.contrib import messages
+import smtplib
 
 # Create your views here.
+
+def inscription(request):
+    my_mail = "julientelook@gmail.com"
+    form = InscriptionForm() # affiche un formulaire vide lorsque arrive sur la page
+    if request.method == 'POST':
+        form = InscriptionForm(request.POST)
+        if(form.is_valid()):
+            internaute = form.save() # => mail de bienvenue
+            try:
+                send_mail(
+                    subject="Bienvenue sur devlog.py",
+                    message=f"Premier mail envoyé à l'utilisateur {internaute.first_name} {internaute.last_name}.",
+                    from_email=f"{my_mail}",
+                    recipient_list=[internaute.email],
+                    fail_silently=False,
+                )
+                messages.success(request, "Tu es maintenant abonné à devlog.py !")
+
+            except BadHeaderError :
+                # Quelqu'un a injecté des headers malveillants dans le sujet ou le message
+                messages.error(request, "Erreur lors de l'envoi du mail.")
+            except smtplib.SMTPException:
+                # Le serveur mail est down, credentials incorrects, etc.
+                messages.error(request,"Erreur lors de l'envoi du mail")
+            return redirect('/')
+    return render(request,'internaute/inscription.html',{'form':form})
+
+
