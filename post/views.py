bd08e9cc2bd476b63d03a7702e548ff0383b0cf1
fork71enthropy <julientelook@gmail.com>
2026-07-12 03:21:08 +0200

    ...

--- post/views.py
+++ post/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404,redirect 
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post,Like_post
 import markdown
 from django.contrib.auth.decorators import login_required
 from comment.forms import CommentForm
