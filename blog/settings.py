d111531ed87d300fdbb488c285192f554d68cc27
fork71enthropy <julientelook@gmail.com>
2026-07-26 13:18:17 +0200

    use database_url for postgresql connection

--- blog/settings.py
+++ blog/settings.py
@@ -109,7 +109,6 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
         conn_max_age=600,
     )
 }
