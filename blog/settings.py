496bffd2c84a8f7c65093b0cfc4b6c34bf682dbf
fork71enthropy <julientelook@gmail.com>
2026-07-26 13:34:02 +0200

    Fix DATABASES config with dj_database_url

--- blog/settings.py
+++ blog/settings.py
@@ -14,7 +14,6 @@ import dj_database_url
 from dotenv import load_dotenv 
 from pathlib import Path
 import os
-import json
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
